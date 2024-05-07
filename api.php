296feb4aadc12de9313a8bc2f0d60786254f0f4d
abbie <abbiereid222@gmail.com>
2024-05-07 16:13:58 +0100

    adding checks

--- api.php
+++ api.php
@@ -65,35 +65,39 @@
             if($oid == null || $name == null || $comment == null) {
                 http_response_code(400);
             } else {
-                $sql = "INSERT INTO apiTable (oid, name, comment) VALUES (?,?,?)";
-                $stmt = $this->conn->prepare($sql);
-                $stmt->bind_param('sss', $oid, $name, $comment); 
-                $stmt->execute();
-
-                if($stmt->affected_rows > 0) {
-                    http_response_code(201);
-
-                    $sql = "SELECT id FROM apiTable WHERE oid = ?";
+                if (strlen($name) > 64 || strlen($oid) > 32) {
+                    http_response_code(400);
+                } else {
+                    $sql = "INSERT INTO apiTable (oid, name, comment) VALUES (?,?,?)";
                     $stmt = $this->conn->prepare($sql);
-                    $stmt->bind_param('s', $oid);
+                    $stmt->bind_param('sss', $oid, $name, $comment); 
                     $stmt->execute();
 
-                    $result = $stmt->get_result();
-                    if ($result->num_rows > 0) {
-                        $row = $result->fetch_assoc();
-                        $response = array('id' => $row['id']);
-                        
-                        header('Content-Type: application/json');
-                        echo json_encode($response);
-                    } else {
-                        http_response_code(500);
-                    }
+                    if($stmt->affected_rows > 0) {
+                        http_response_code(201);
 
-                } else {
-                    http_response_code(500); 
+                        $sql = "SELECT id FROM apiTable WHERE oid = ?";
+                        $stmt = $this->conn->prepare($sql);
+                        $stmt->bind_param('s', $oid);
+                        $stmt->execute();
+
+                        $result = $stmt->get_result();
+                        if ($result->num_rows > 0) {
+                            $row = $result->fetch_assoc();
+                            $response = array('id' => $row['id']);
+                            
+                            header('Content-Type: application/json');
+                            echo json_encode($response);
+                        } else {
+                            http_response_code(500);
+                        }
+
+                    } else {
+                        http_response_code(500); 
+                    }
                 }
-            }
         }
+    }
 
         function __destruct() {
             $this->conn->close();
